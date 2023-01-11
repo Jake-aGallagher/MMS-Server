64ce4488ead92211f7b4bb1abffc1de640e6f86f
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-01-11 23:07:13 +0000

    Added dotenv package

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-const express = require("express");
+require('dotenv').config();
+console.log("read env variables", process.env.DATABASE)
+const express = require('express');
 
 const app = express();
 
 app.listen(3001, () => {
-    console.log("server is running");
-  });
+    console.log('server is running');
+});
