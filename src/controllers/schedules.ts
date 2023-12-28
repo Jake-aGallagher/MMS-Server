35f070a69c641008bce8974f3dd7152206f12b52
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-28 19:00:28 +0000

    Get Schedule details controller method

--- src/controllers/schedules.ts
+++ src/controllers/schedules.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 import * as Schedules from '../models/schedules';
 import * as TypeEnums from '../models/jobTypes';
+import * as Users from '../models/users';
+import * as Spares from '../models/spares';
+import * as LoggedTime from '../models/loggedTime';
+import makeIdList from '../helpers/makeIdList';
+import timeDetailsArray from '../helpers/jobs/timeDetailsArray';
 
 export async function getAllSchedules(req: Request, res: Response) {
     try {
@@ -11,6 +16,27 @@ export async function getAllSchedules(req: Request, res: Response) {
         console.log(err);
         res.status(500).json({ message: 'Request failed' });
     }
+}
+
+export async function getScheduleDetails(req: Request, res: Response) {
+    try {
+        const id = parseInt(req.params.scheduleid);
+        const scheduleDetails = await Schedules.getScheduleDetails(id);
+        const usedSpares = await Spares.getUsedSpares('schedule', id);
+        const timeDetails = await LoggedTime.getLoggedTimeDetails('schedule', id);
+        if (timeDetails.length > 0) {
+            const userIds = makeIdList(timeDetails, 'id');
+            const users = await Users.getUsersByIds(userIds);
+            const timeDetailsFull = timeDetailsArray(timeDetails, users);
+            res.status(200).json({ scheduleDetails, usedSpares, timeDetails: timeDetailsFull });
+        } else {
+            res.status(200).json({ scheduleDetails, usedSpares });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Request failed' });
+    }
+
 }
 
 export async function getAddScheduleEnums(req: Request, res: Response) {
