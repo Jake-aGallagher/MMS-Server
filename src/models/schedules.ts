27aebf9cafa52b2c5ef1531b88cf00f9920a1f06
Jake Gallagher <jake.a.gallagher98@gmail.com>
2023-12-28 18:59:58 +0000

    get Schedule Details method

--- src/models/schedules.ts
+++ src/models/schedules.ts
@@ -43,6 +43,51 @@ export async function getAllSchedules(propertyId: number) {
     return data[0];
 }
 
+export async function getScheduleDetails(scheduleId: number) {
+    const data = await db.execute(
+        `SELECT 
+                schedules.id,
+                properties.name AS property,
+                IF (LENGTH(assets.name) > 0, assets.name, 'No Asset') AS asset,
+                typeEnum.value AS type,
+                schedules.title,
+                schedules.description,
+                schedules.notes,
+                DATE_FORMAT(schedules.created, "%d/%m/%y") AS 'created',
+                DATE_FORMAT(schedules.required_comp_date, "%d/%m/%y") AS 'required_comp_date',
+                schedules.completed,
+                DATE_FORMAT(schedules.comp_date, "%d/%m/%y") AS 'comp_date',
+                schedules.logged_time,
+                statusEnum.value AS status,
+                CONCAT(
+                    schedules.frequency_time,
+                    ' ',
+                    CASE
+                        WHEN schedules.frequency_unit = 'DAY' THEN 'Day(s)'
+                        WHEN schedules.frequency_unit = 'WEEK' THEN 'Week(s)'
+                        WHEN schedules.frequency_unit = 'MONTH' THEN 'Month(s)'
+                        WHEN schedules.frequency_unit = 'YEAR' THEN 'Year(s)'
+                    END
+                ) AS frequency
+            FROM 
+                schedules
+            LEFT JOIN assets ON
+                assets.id = schedules.asset_id
+            LEFT JOIN job_types AS typeEnum ON
+                schedules.type = typeEnum.id
+            LEFT JOIN status_types AS statusEnum ON
+                schedules.status = statusEnum.id
+            LEFT JOIN properties ON
+                properties.id = schedules.property_id
+            WHERE
+                schedules.id = ?
+            AND
+                schedules.deleted = 0;`,
+        [scheduleId]
+    );
+    return data[0];
+}
+
 export async function addSchedule(body: any) {
     const initialStatus: [InitialStatus[], FieldPacket[]] = await db.execute("SELECT id FROM status_types WHERE initial_status = '1' LIMIT 1");
     const dueDate = body.startNow === 'Yes' ? 'CURDATE()' : body.scheduleStart;
