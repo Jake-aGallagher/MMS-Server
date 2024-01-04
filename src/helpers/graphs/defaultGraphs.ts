8faa3aba84ebb5b044fe487a0f6786d34c60c0f2
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-01-04 23:28:27 +0000

    Jobs completed uses Schedules

--- src/helpers/graphs/defaultGraphs.ts
+++ src/helpers/graphs/defaultGraphs.ts
@@ -3,7 +3,7 @@ import { FieldPacket } from 'mysql2/typings/mysql';
 import { DefaultGraph6M, IncompleteJobs, NameValue } from '../../types/defaultGraphs';
 import { monthsLooped } from './monthsLooped';
 
-export async function getIncompleteJobs(propertyId: number) {// pm
+export async function getIncompleteJobs(propertyId: number) {
     const data: [IncompleteJobs[], FieldPacket[]] = await db.execute(
         `SELECT
             COUNT(IF(completed = 0 AND required_comp_date > CURDATE(), 1, NULL)) AS incomplete,
@@ -29,7 +29,7 @@ export async function getIncompleteJobs(propertyId: number) {// pm
     return data[0];
 }
 
-export async function getJobsRaised6M(propertyId: number) {
+export async function getJobsRaised6M(propertyId: number) {// pm
     const d = new Date();
     const endNum = d.getMonth();
     let startNum = endNum >= 5 ? endNum - 5 : 7 + endNum;
@@ -75,16 +75,32 @@ export async function getJobsCompleted6M(propertyId: number) {// pm
         FROM
             jobs
         WHERE
-            property_id = ?;`,
-        [propertyId]
+            property_id = ?
+            
+        UNION
+        
+        SELECT
+            COUNT(IF(MONTHNAME(comp_date) = "${monthsLooped[startNum]}" && comp_date > DATE_SUB(NOW(), INTERVAL 7 MONTH), 1, NULL)) AS month_1,
+            COUNT(IF(MONTHNAME(comp_date) = "${monthsLooped[startNum + 1]}" && comp_date > DATE_SUB(NOW(), INTERVAL 7 MONTH), 1, NULL)) AS month_2,
+            COUNT(IF(MONTHNAME(comp_date) = "${monthsLooped[startNum + 2]}" && comp_date > DATE_SUB(NOW(), INTERVAL 7 MONTH), 1, NULL)) AS month_3,
+            COUNT(IF(MONTHNAME(comp_date) = "${monthsLooped[startNum + 3]}" && comp_date > DATE_SUB(NOW(), INTERVAL 7 MONTH), 1, NULL)) AS month_4,
+            COUNT(IF(MONTHNAME(comp_date) = "${monthsLooped[startNum + 4]}" && comp_date > DATE_SUB(NOW(), INTERVAL 7 MONTH), 1, NULL)) AS month_5,
+            COUNT(IF(MONTHNAME(comp_date) = "${monthsLooped[startNum + 5]}" && comp_date > DATE_SUB(NOW(), INTERVAL 7 MONTH), 1, NULL)) AS month_6
+        FROM
+            schedules
+        INNER JOIN schedule_templates ON
+            schedules.template_id = schedule_templates.id
+        WHERE
+            schedule_templates.property_id = ?;`,
+        [propertyId, propertyId]
     );
     const returnObj = [
-        { month: monthsLooped[startNum], value: data[0][0].month_1 },
-        { month: monthsLooped[startNum + 1], value: data[0][0].month_2 },
-        { month: monthsLooped[startNum + 2], value: data[0][0].month_3 },
-        { month: monthsLooped[startNum + 3], value: data[0][0].month_4 },
-        { month: monthsLooped[startNum + 4], value: data[0][0].month_5 },
-        { month: monthsLooped[startNum + 5], value: data[0][0].month_6 },
+        { month: monthsLooped[startNum], value: data[0][0].month_1 + data[0][1].month_1 },
+        { month: monthsLooped[startNum + 1], value: data[0][0].month_2 + data[0][1].month_2 },
+        { month: monthsLooped[startNum + 2], value: data[0][0].month_3 + data[0][1].month_3 },
+        { month: monthsLooped[startNum + 3], value: data[0][0].month_4 + data[0][1].month_4 },
+        { month: monthsLooped[startNum + 4], value: data[0][0].month_5 + data[0][1].month_5 },
+        { month: monthsLooped[startNum + 5], value: data[0][0].month_6 + data[0][1].month_6 },
     ];
     return returnObj;
 }
