38214ac39f860dd11c2cb63fd9ac277a6bfa7534
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-05-05 18:41:54 +0100

    Change multer from disk to azure storage

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -1,16 +1,4 @@
-import multer from "multer";
-import { v4 as uuidv4 } from 'uuid';
-const DIR = process.env.FILE_DEST || 'uploads/';
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, DIR);
-    },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname.toLowerCase().split(' ').join('-');
-        cb(null, uuidv4() + '__' + fileName)
-    }
-});
+import multer from 'multer';
 
 const acceptableTypes = [
     'application/msword',
@@ -21,15 +9,15 @@ const acceptableTypes = [
     'application/pdf',
     'application/vnd.ms-excel',
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-]
+];
 
 export const fileUpload = multer({
-    storage: storage,
+    storage: multer.memoryStorage(),
     fileFilter: (req, file, cb) => {
         if (acceptableTypes.includes(file.mimetype)) {
             cb(null, true);
         } else {
             cb(null, false);
         }
-    }
+    },
 });
