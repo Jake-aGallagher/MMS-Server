a6ce2836513787bdca6a71e539e117be7d888adf
Jake Gallagher <jake.a.gallagher98@gmail.com>
2024-08-16 11:06:51 +0100

    Handle updating of responses inside of a transaction

--- src/models/audit/auditResponses.ts
+++ src/models/audit/auditResponses.ts
@@ -8,6 +8,7 @@ export async function getAuditResponses(client: string, auditId: number) {
         `SELECT
             id,
             audit_id,
+            question_id,
             response
         FROM
             audit_responses
@@ -18,20 +19,39 @@ export async function getAuditResponses(client: string, auditId: number) {
     return data[0];
 }
 
-export async function addResponses(client: string, auditId: number, responses: AuditResponseForInsert[]) {
+export async function updateResponses(client: string, auditId: number, responses: AuditResponseForInsert[]) {
     const db = await getConnection('client_' + client);
-    const responseArray = responses.map((r) => {return [auditId, r.question_id, r.response]});
-    await db.execute(
-        `INSERT INTO
+    try {
+        const conn = await db.getConnection();
+        await conn.beginTransaction();
+        await conn.execute(
+            `DELETE FROM
             audit_responses
-            (
-                audit_id,
-                question_id,
-                response
-            )
-        VALUES
-            ?;`,
-            [responseArray]
-    );
-    return;
+        WHERE
+            audit_id = ?;`,
+        [auditId]
+        );
+        const responseArray = responses.map((r) => {return [auditId, r.question_id, r.response]});
+        if (responseArray.length > 0) {
+            const sqlInsert = db.format(
+                `INSERT INTO
+                    audit_responses
+                    (
+                        audit_id,
+                        question_id,
+                        response
+                    )
+                VALUES
+                    ?;`,
+                    [responseArray]
+            );
+            await conn.execute(sqlInsert);
+        }
+        await conn.commit();
+        conn.release();
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+    return true;
 }
